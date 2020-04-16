804497c7fa288fe6d5b5078ae96398642c3e4558
katagis <fox.dealos@gmail.com>
2020-04-16 15:14:15 +0300

    Initial commit

---
+++ src/main/java/estatehub/code/EstateHubApplication.java
@@ -0,0 +1,13 @@
+package estatehub.code;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstateHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstateHubApplication.class, args);
+	}
+
+}
