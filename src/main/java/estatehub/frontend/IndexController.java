5b686fca0432b16adef695b0baf9eddd2f3046ca
katagis <fox.dealos@gmail.com>
2020-04-16 23:34:57 +0300

    Template Example with header

---
+++ src/main/java/estatehub/frontend/IndexController.java
@@ -0,0 +1,25 @@
+package estatehub.frontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    public String homePage() {
+        return "index";
+    }
+
+    @GetMapping("/contact")
+    public String contactPage() {
+        return "contact";
+    }
+
+    @GetMapping("/about")
+    public String aboutPage() {
+        return "about";
+    }
+}
