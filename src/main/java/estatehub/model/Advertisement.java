5b0410cf3a261a2f593900165c2f4efdc5b6058e
katagis <fox.dealos@gmail.com>
2020-04-27 16:21:02 +0300

    Add advertisement class

---
+++ src/main/java/estatehub/model/Advertisement.java
@@ -0,0 +1,14 @@
+package estatehub.model;
+
+public class Advertisement {
+    private Estate estate;
+    private int duration;
+    
+    public Estate getEstate() {
+        return estate;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+}
