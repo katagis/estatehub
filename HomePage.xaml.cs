866fc648d930cc74d5a48eb6e23291abde858219
katagis <fox.dealos@gmail.com>
2020-05-02 17:08:14 +0300

    Replace with C# code

---
+++ HomePage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EstateHub
+{
+    /// <summary>
+    /// Interaction logic for SelectionPage.xaml
+    /// </summary>
+    public partial class SelectionPage : Page
+    {
+        public SelectionPage()
+        {
+            InitializeComponent();
+        }
+
+        private void butto2n_Click(object sender, RoutedEventArgs e)
+        {
+            (Application.Current.MainWindow as MainWindow).ChangeView("Selection2.xaml");
+            
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EstateHub
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow() {
+            InitializeComponent();
+        }
+
+        private void homeBtn_Click(object sender, RoutedEventArgs e) {
+            ChangeView("HomePage.xaml");
+        }
+
+        public void ChangeView(string view) {
+            ui_mainView.Source = new Uri(view, UriKind.Relative);
+        }
+
+    }
+}
