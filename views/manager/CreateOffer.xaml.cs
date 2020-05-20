49440717ba949d766c8d4460e723dadd5df41482
katagis <fox.dealos@gmail.com>
2020-05-20 16:11:32 +0300

    Add create offer view

---
+++ views/manager/CreateOffer.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Printing;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using EstateHub.model;
+
+namespace EstateHub.views.manager
+{
+    /// <summary>
+    /// Interaction logic for CreateOffer.xaml
+    /// </summary>
+    public partial class CreateOffer : Page
+    {
+        private Estate estate;
+
+        public CreateOffer() {
+            InitializeComponent();
+        }
+
+        private void ui_submit_Click(object sender, RoutedEventArgs e) {
+
+        }
+
+        private void ui_seasons_TextChanged(object sender, TextChangedEventArgs e) {
+
+        }
+
+        private void ui_amount_TextChanged(object sender, TextChangedEventArgs e) {
+
+        }
+    }
+}
