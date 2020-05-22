67f0a6d183735e231602c11178ad8b96f6514b29
katagis <fox.dealos@gmail.com>
2020-05-22 15:02:49 +0300

    Add xaml views for advertisement use case

---
+++ views/owner/AdvertiseEstate.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EstateHub.views.owner
+{
+    /// <summary>
+    /// Interaction logic for AdvertiseEstate.xaml
+    /// </summary>
+    public partial class AdvertiseEstate : Page
+    {
+        public AdvertiseEstate() {
+            InitializeComponent();
+        }
+
+        private void ui_seasons_TextChanged(object sender, TextChangedEventArgs e) {
+
+        }
+
+        private void ui_submit_Click(object sender, RoutedEventArgs e) {
+
+        }
+
+        private void ui_cardnumber_TextChanged(object sender, TextChangedEventArgs e) {
+
+        }
+    }
+}
